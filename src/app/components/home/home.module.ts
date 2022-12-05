b3b5618e0fe490e56fb90329c471b48d85324bc4
chatreejs <chanon29060@live.com>
2022-12-06 05:54:51 +0700

    feat: Add payer list mockup

--- src/app/components/home/home.module.ts
+++ src/app/components/home/home.module.ts
@@ -2,19 +2,24 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { NzButtonModule } from 'ng-zorro-antd/button';
+import { NzCardModule } from 'ng-zorro-antd/card';
+import { NzCheckboxModule } from 'ng-zorro-antd/checkbox';
 import { NzDividerModule } from 'ng-zorro-antd/divider';
 import { NzFormModule } from 'ng-zorro-antd/form';
+import { NzGridModule } from 'ng-zorro-antd/grid';
 import { NzIconModule } from 'ng-zorro-antd/icon';
 import { NzInputModule } from 'ng-zorro-antd/input';
 import { NzModalModule } from 'ng-zorro-antd/modal';
 import { NzPopconfirmModule } from 'ng-zorro-antd/popconfirm';
 import { NzTableModule } from 'ng-zorro-antd/table';
 import { NzTabsModule } from 'ng-zorro-antd/tabs';
+import { NzTagModule } from 'ng-zorro-antd/tag';
 
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home.component';
 import { ItemListModalComponent } from './item-list/item-list-modal/item-list-modal.component';
 import { ItemListComponent } from './item-list/item-list.component';
+import { PayerListModalComponent } from './payer-list/payer-list-modal/payer-list-modal.component';
 import { PayerListComponent } from './payer-list/payer-list.component';
 
 @NgModule({
@@ -23,6 +28,7 @@ import { PayerListComponent } from './payer-list/payer-list.component';
     ItemListComponent,
     PayerListComponent,
     ItemListModalComponent,
+    PayerListModalComponent,
   ],
   imports: [
     CommonModule,
@@ -39,6 +45,10 @@ import { PayerListComponent } from './payer-list/payer-list.component';
     NzDividerModule,
     NzFormModule,
     NzPopconfirmModule,
+    NzTagModule,
+    NzCheckboxModule,
+    NzCardModule,
+    NzGridModule,
   ],
 })
 export class HomeModule {}
--- src/app/components/home/payer-list/payer-list.component.ts
+++ src/app/components/home/payer-list/payer-list.component.ts
@@ -1,27 +1,125 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewContainerRef } from '@angular/core';
+import { NzModalService } from 'ng-zorro-antd/modal';
 import { ModalType } from 'src/app/common/enums';
 import { Payer } from 'src/app/common/models';
 import { v4 as uuidv4 } from 'uuid';
+import { PayerListModalComponent } from './payer-list-modal/payer-list-modal.component';
 @Component({
   selector: 'app-payer-list',
   templateUrl: './payer-list.component.html',
   styleUrls: ['./payer-list.component.scss'],
 })
 export class PayerListComponent implements OnInit {
+  isShowCheckbox: boolean = false;
+  isAllChecked: boolean = false;
+  isIndeterminate: boolean = false;
   listOfPayer: Payer[] = [
     {
       id: uuidv4(),
       name: 'ชานนท์',
+      children: [
+        {
+          id: uuidv4(),
+          name: 'อุ้ย',
+        },
+        {
+          id: uuidv4(),
+          name: 'เค',
+        },
+      ],
+    },
+    {
+      id: uuidv4(),
+      name: 'ต้นตุง',
+      children: [],
     },
   ];
+  setOfCheckedId: Set<string> = new Set<string>();
+  expandSet = new Set<string>();
 
   get modalType(): typeof ModalType {
     return ModalType;
   }
 
-  constructor() {}
+  constructor(
+    private modalService: NzModalService,
+    private viewContainerRef: ViewContainerRef,
+  ) {}
 
   ngOnInit(): void {}
 
-  openPayerListModal(type: ModalType, id?: string): void {}
+  toggleCheckbox(): void {
+    this.isShowCheckbox = !this.isShowCheckbox;
+    if (!this.isShowCheckbox) {
+      this.setOfCheckedId.clear();
+      this.refreshCheckedStatus();
+    }
+  }
+
+  updateCheckedSet(id: string, checked: boolean): void {
+    if (checked) {
+      this.setOfCheckedId.add(id);
+    } else {
+      this.setOfCheckedId.delete(id);
+    }
+  }
+
+  refreshCheckedStatus(): void {
+    this.isAllChecked = this.listOfPayer.every((item) =>
+      this.setOfCheckedId.has(item.id),
+    );
+    this.isIndeterminate =
+      this.listOfPayer.some((item) => this.setOfCheckedId.has(item.id)) &&
+      !this.isAllChecked;
+  }
+
+  onItemChecked(id: string, checked: boolean): void {
+    this.updateCheckedSet(id, checked);
+    this.refreshCheckedStatus();
+  }
+
+  onAllChecked(checked: boolean): void {
+    this.listOfPayer.forEach((item) => this.updateCheckedSet(item.id, checked));
+    this.refreshCheckedStatus();
+  }
+
+  onExpandChange(id: string, checked: boolean): void {
+    if (checked) {
+      this.expandSet.add(id);
+    } else {
+      this.expandSet.delete(id);
+    }
+  }
+
+  deleteItemList(): void {
+    console.log(this.setOfCheckedId);
+    this.toggleCheckbox();
+  }
+
+  openPayerListModal(type: ModalType, id?: string): void {
+    this.modalService.create({
+      nzTitle: type === ModalType.Create ? 'เพิ่มคนจ่าย' : 'แก้ไขคนจ่าย',
+      nzContent: PayerListModalComponent,
+      nzViewContainerRef: this.viewContainerRef,
+      nzComponentParams: {
+        type,
+        id,
+      },
+      nzFooter: [
+        {
+          label: 'ยกเลิก',
+          onClick: (component) => {
+            component?.destroyModal();
+          },
+        },
+        {
+          label: 'บันทึก',
+          type: 'primary',
+          onClick: (component) => {
+            component?.savePayer();
+          },
+        },
+      ],
+    });
+  }
 }
