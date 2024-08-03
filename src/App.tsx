c423b83b53a45120aaeaf2fa3a0f43e73b9ddd2f
Chatree.js <36321701+chatreejs@users.noreply.github.com>
2024-08-03 20:18:34 +0700

    refactor: refactor all project to react

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { useState } from 'react';
+
+const App: React.FC = () => {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <h1>Hi Mom</h1>
+    </>
+  );
+};
+
+export default App;
