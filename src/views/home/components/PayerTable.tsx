6a53451685c301220d454b3abc7fd6c27fcf2acc
Chatree.js <36321701+chatreejs@users.noreply.github.com>
2024-08-17 17:38:30 +0700

    feat: add button

--- src/views/home/components/PayerTable.tsx
+++ src/views/home/components/PayerTable.tsx
@@ -1,7 +1,27 @@
+import { PlusOutlined } from '@ant-design/icons';
+import { Button } from 'antd';
 import React from 'react';
+import styled from 'styled-components';
+
+const ActionButton = styled.div``;
+
+const ButtonWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 0.5rem;
+`;
 
 const PayerTable: React.FC = () => {
-  return <div>PayerTable</div>;
+  return (
+    <>
+      <ActionButton></ActionButton>
+      <ButtonWrapper>
+        <Button type="dashed" size="large" icon={<PlusOutlined />}>
+          Add Payer
+        </Button>
+      </ButtonWrapper>
+    </>
+  );
 };
 
 export default PayerTable;
