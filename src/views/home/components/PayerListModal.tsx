78e2086d06e11282ddbf628e979e7c32f59bf501
Chatree.js <36321701+chatreejs@users.noreply.github.com>
2024-08-18 21:25:27 +0700

    feat: add payer

---
+++ src/views/home/components/PayerListModal.tsx
@@ -0,0 +1,195 @@
+import { Button, Card, Checkbox, Flex, Form, Input } from 'antd';
+import Modal from 'antd/es/modal/Modal';
+import React, { useEffect } from 'react';
+import { useTranslation } from 'react-i18next';
+import { useDispatch, useSelector } from 'react-redux';
+import styled from 'styled-components';
+import { v4 as uuidv4 } from 'uuid';
+
+import { DeleteOutlined, PlusOutlined } from '@ant-design/icons';
+import { RootState } from '@config';
+import { ModalType } from '@enums';
+import { IPayerForm } from '@interfaces';
+import { addPayer, editPayer, removePayer } from '@slices';
+
+const FormWrapper = styled.div`
+  margin-top: 1rem;
+  display: flex;
+  flex-direction: column;
+`;
+
+interface Props {
+  mode: ModalType;
+  isOpen: boolean;
+  payerId?: string;
+  onClose: () => void;
+}
+
+const PayerListModal: React.FC<Props> = ({
+  mode,
+  isOpen,
+  payerId,
+  onClose,
+}) => {
+  const { t } = useTranslation();
+  const billPayers = useSelector((state: RootState) => state.bill.payers);
+  const dispatch = useDispatch();
+  const [form] = Form.useForm<IPayerForm>();
+  const [isHasFriend, setIsHasFriend] = React.useState<boolean>(false);
+
+  const initialValues: IPayerForm = {
+    name: '',
+    friend: [],
+  };
+
+  const savePayer = () => {
+    form
+      .validateFields()
+      .then(() => {
+        const formData = form.getFieldsValue(true) as IPayerForm;
+        if (!isHasFriend) {
+          formData.friend = [];
+        } else {
+          formData.friend = formData.friend.map((item) => ({
+            id: item.id || uuidv4(),
+            name: item.name,
+          }));
+        }
+        if (mode === ModalType.Create) {
+          dispatch(addPayer({ ...formData, id: uuidv4() }));
+        } else {
+          dispatch(editPayer({ ...formData, id: payerId }));
+        }
+        onClose();
+      })
+      .catch((error) => {
+        return;
+      });
+  };
+
+  const deletePayer = () => {
+    dispatch(removePayer(payerId));
+    onClose();
+  };
+
+  useEffect(() => {
+    if (isOpen) {
+      if (mode === ModalType.Edit && payerId) {
+        const payer = billPayers.find((item) => item.id === payerId);
+        form.setFieldsValue(payer);
+        if (payer.friend.length > 0) {
+          setIsHasFriend(true);
+        }
+      } else {
+        form.setFieldsValue(initialValues);
+      }
+    }
+
+    return () => {
+      form.resetFields();
+      setIsHasFriend(false);
+    };
+  }, [isOpen, mode, payerId]);
+
+  return (
+    <Modal
+      title={
+        mode === ModalType.Create
+          ? t('home.payerList.modal.title.add')
+          : t('home.payerList.modal.title.edit')
+      }
+      open={isOpen}
+      onOk={() => savePayer()}
+      onCancel={() => onClose()}
+      okText={t('common.button.save')}
+      cancelText={t('common.button.cancel')}
+    >
+      <FormWrapper>
+        <Form name="payerListForm" form={form} layout="vertical" size="middle">
+          <Form.Item
+            name="name"
+            label={t('home.payerList.modal.form.name')}
+            rules={[
+              {
+                required: true,
+                message: t('home.payerList.modal.form.nameError'),
+              },
+            ]}
+          >
+            <Input />
+          </Form.Item>
+          <Checkbox
+            checked={isHasFriend}
+            onChange={() => setIsHasFriend(!isHasFriend)}
+          >
+            {t('home.payerList.modal.form.hasChildren')}
+          </Checkbox>
+          {isHasFriend && (
+            <Card style={{ marginTop: '1rem', marginBottom: '1rem' }}>
+              <Form.List name="friend">
+                {(fields, { add, remove }) => (
+                  <>
+                    {fields.map(({ key, name, ...restField }) => (
+                      <Flex key={key} justify="space-between">
+                        <Form.Item
+                          {...restField}
+                          key={key}
+                          name={[name, 'name']}
+                          rules={[
+                            {
+                              required: true,
+                              message: t(
+                                'home.payerList.modal.form.childrenNameError',
+                              ),
+                            },
+                          ]}
+                        >
+                          <Input
+                            size="middle"
+                            placeholder={t(
+                              'home.payerList.modal.form.childrenName',
+                            )}
+                          />
+                        </Form.Item>
+                        <Button
+                          type="default"
+                          size="middle"
+                          onClick={() => remove(name)}
+                          icon={<DeleteOutlined />}
+                          danger
+                        />
+                      </Flex>
+                    ))}
+                    <Form.Item>
+                      <Button
+                        type="dashed"
+                        onClick={() => add()}
+                        block
+                        icon={<PlusOutlined />}
+                      >
+                        {t('home.payerList.modal.form.addChildren')}
+                      </Button>
+                    </Form.Item>
+                  </>
+                )}
+              </Form.List>
+            </Card>
+          )}
+        </Form>
+        {mode === ModalType.Edit && (
+          <Button
+            type="primary"
+            size="middle"
+            icon={<DeleteOutlined />}
+            onClick={() => deletePayer()}
+            danger
+          >
+            {t('home.payerList.modal.form.delete')}
+          </Button>
+        )}
+      </FormWrapper>
+    </Modal>
+  );
+};
+
+export default PayerListModal;
--- src/views/home/components/PayerTable.tsx
+++ src/views/home/components/PayerTable.tsx
@@ -1,25 +1,162 @@
 import { PlusOutlined } from '@ant-design/icons';
-import { Button } from 'antd';
-import React from 'react';
-import styled from 'styled-components';
-
-const ActionButton = styled.div``;
-
-const ButtonWrapper = styled.div`
-  display: flex;
-  flex-direction: column;
-  gap: 0.5rem;
-`;
+import { RootState } from '@config';
+import { ModalType } from '@enums';
+import { IPayer } from '@interfaces';
+import { removeAllPayers, removePayers } from '@slices';
+import { Button, Popconfirm, Table, Tag } from 'antd';
+import Column from 'antd/es/table/Column';
+import { TableRowSelection } from 'antd/es/table/interface';
+import React, { useState } from 'react';
+import { useTranslation } from 'react-i18next';
+import { useDispatch, useSelector } from 'react-redux';
+import { ActionButton, ButtonWrapper, TableWrapper } from '../Home';
+import PayerListModal from './PayerListModal';
 
 const PayerTable: React.FC = () => {
+  const { t } = useTranslation();
+  const billPayers = useSelector((state: RootState) => state.bill.payers);
+  const dispatch = useDispatch();
+
+  const [isShowCheckbox, setIsShowCheckbox] = useState<boolean>(false);
+  const [selectedRowKeys, setSelectedRowKeys] =
+    useState<React.Key[]>(undefined);
+
+  const [isModalVisible, setIsModalVisible] = useState<boolean>(false);
+  const [modalMode, setModalMode] = useState<ModalType>(ModalType.Create);
+  const [selectedPayerId, setSelectedPayerId] = useState<string>(undefined);
+
+  console.log(billPayers);
+
+  const toggleCheckbox = () => {
+    setIsShowCheckbox(!isShowCheckbox);
+    setSelectedRowKeys([]);
+  };
+
+  const onSelectChange = (newSelectedRowKeys: React.Key[]) => {
+    setSelectedRowKeys(newSelectedRowKeys);
+  };
+
+  const rowSelection: TableRowSelection<IPayer> = {
+    selectedRowKeys,
+    onChange: onSelectChange,
+  };
+
+  const deletePayerList = () => {
+    if (selectedRowKeys.length === 0) {
+      dispatch(removeAllPayers());
+      setSelectedRowKeys([]);
+    } else if (selectedRowKeys?.length > 0) {
+      const idList = selectedRowKeys.map((key) => key.toString());
+      dispatch(removePayers(idList));
+      setSelectedRowKeys([]);
+    }
+  };
+
+  const openModal = (mode: ModalType, id?: string) => {
+    setModalMode(mode);
+    setIsModalVisible(true);
+    setSelectedPayerId(id);
+  };
+
+  const closeModal = () => {
+    setIsModalVisible(false);
+    setSelectedPayerId(undefined);
+  };
+
   return (
     <>
-      <ActionButton></ActionButton>
+      {billPayers.length > 0 && (
+        <ActionButton>
+          {isShowCheckbox && (
+            <Popconfirm
+              title={t('home.payerList.popup.deleteTitle')}
+              description={
+                selectedRowKeys.length === 0
+                  ? t('home.payerList.popup.deleteAll')
+                  : t('home.payerList.popup.deleteSelected', {
+                      count: selectedRowKeys.length,
+                    })
+              }
+              onConfirm={deletePayerList}
+              okText={t('common.button.ok')}
+              cancelText={t('common.button.cancel')}
+            >
+              <Button danger>
+                {selectedRowKeys.length > 0
+                  ? t('home.payerList.button.deleteSelected')
+                  : t('home.payerList.button.deleteAll')}
+              </Button>
+            </Popconfirm>
+          )}
+          <Button type="default" onClick={toggleCheckbox}>
+            {isShowCheckbox
+              ? t('common.button.cancel')
+              : t('home.payerList.button.select')}
+          </Button>
+        </ActionButton>
+      )}
+      {billPayers.length > 0 && (
+        <TableWrapper>
+          <Table
+            dataSource={billPayers}
+            pagination={false}
+            expandable={{
+              expandedRowRender: (record) => {
+                return (
+                  <div>
+                    {record.friend.map((friend) => (
+                      <Tag key={friend.id}>{friend.name}</Tag>
+                    ))}
+                  </div>
+                );
+              },
+              rowExpandable: (record) => record.friend.length > 0,
+            }}
+            rowSelection={isShowCheckbox ? rowSelection : undefined}
+            rowKey={(record) => record.id}
+            onRow={(record) => {
+              return {
+                onClick: () => {
+                  openModal(ModalType.Edit, record.id);
+                },
+              };
+            }}
+          >
+            <Column
+              title={t('home.payerList.table.name')}
+              key="name"
+              dataIndex="name"
+              render={(text, record: IPayer) => {
+                if (record.friend.length === 0) {
+                  return <span>{text}</span>;
+                } else {
+                  return (
+                    <span>
+                      {text} {t('home.payerList.table.andFriend')}
+                    </span>
+                  );
+                }
+              }}
+            />
+          </Table>
+        </TableWrapper>
+      )}
       <ButtonWrapper>
-        <Button type="dashed" size="large" icon={<PlusOutlined />}>
-          Add Payer
+        <Button
+          type="dashed"
+          size="large"
+          icon={<PlusOutlined />}
+          onClick={() => openModal(ModalType.Create)}
+        >
+          {t('home.payerList.button.add')}
         </Button>
       </ButtonWrapper>
+      <PayerListModal
+        mode={modalMode}
+        isOpen={isModalVisible}
+        onClose={closeModal}
+        payerId={selectedPayerId}
+      />
     </>
   );
 };
