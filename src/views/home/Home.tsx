0585d1ebb8b9e764d327f68974d307b8d181da28
Chatree.js <36321701+chatreejs@users.noreply.github.com>
2024-08-22 15:12:53 +0700

    feat: disable next button when no record

--- src/views/home/Home.tsx
+++ src/views/home/Home.tsx
@@ -7,7 +7,7 @@ import { BillCard, ButtonWrapper } from '@components';
 import { RootState } from '@config';
 import { setTitle } from '@slices';
 import { Button, Input, Tabs, TabsProps } from 'antd';
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useTranslation } from 'react-i18next';
 import { useDispatch, useSelector } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
@@ -18,8 +18,12 @@ const Home: React.FC = () => {
   const { t } = useTranslation();
   const navigate = useNavigate();
   const billTitle = useSelector((state: RootState) => state.bill.title);
+  const billItems = useSelector((state: RootState) => state.bill.items);
+  const billPayers = useSelector((state: RootState) => state.bill.payers);
   const dispatch = useDispatch();
   const [activeTab, setActiveTab] = useState<string>('item');
+  const [isNextButtonDisabled, setIsNextButtonDisabled] =
+    useState<boolean>(true);
 
   const tabItems: TabsProps['items'] = [
     {
@@ -43,6 +47,14 @@ const Home: React.FC = () => {
   const onTabClick = (key: string) => {
     setActiveTab(key);
   };
+
+  useEffect(() => {
+    if (billItems.length > 0 && billPayers.length > 0) {
+      setIsNextButtonDisabled(false);
+    } else {
+      setIsNextButtonDisabled(true);
+    }
+  }, [billItems, billPayers]);
 
   return (
     <BillCard
@@ -78,6 +90,7 @@ const Home: React.FC = () => {
             onClick={() => {
               navigate('/mapping');
             }}
+            disabled={isNextButtonDisabled}
           >
             {t('common.button.next')}
             <RightOutlined />
