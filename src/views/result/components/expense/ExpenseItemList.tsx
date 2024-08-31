af65b5f4565da35e4a1111486df51cbd55f975ae
Chatree.js <36321701+chatreejs@users.noreply.github.com>
2024-08-31 23:57:31 +0700

    feat: decoupling expense list

---
+++ src/views/result/components/expense/ExpenseItemList.tsx
@@ -0,0 +1,40 @@
+import { Flex } from 'antd';
+import React from 'react';
+import styled from 'styled-components';
+
+import { IExpense, IExpenseChildren } from '@interfaces';
+
+const ItemWrapper = styled.div`
+  display: flex;
+  justify-content: space-between;
+
+  div > span:first-child {
+    margin-right: 1rem;
+  }
+
+  font-size: 13px;
+`;
+
+interface Props {
+  expense: IExpense | IExpenseChildren;
+}
+
+const ExpenseItemList: React.FC<Props> = ({ expense }) => {
+  return (
+    <>
+      {expense.items.map((item) => {
+        return (
+          <ItemWrapper key={item.itemName}>
+            <Flex gap={'12px'}>
+              <div>{item.itemQuantity}</div>
+              <div>{item.itemName}</div>
+            </Flex>
+            <div>{item.itemTotalPrice}</div>
+          </ItemWrapper>
+        );
+      })}
+    </>
+  );
+};
+
+export default ExpenseItemList;
